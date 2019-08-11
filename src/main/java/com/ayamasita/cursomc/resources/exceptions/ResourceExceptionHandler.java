fbc735f509a59a113355cbf6a1ffc72c14146dac
ayamasita <alexandreyamasita@gmail.com>
2019-08-11 17:58:23 -0300

    Ajustes no endpoint /categorias/{id}

---
+++ src/main/java/com/ayamasita/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.ayamasita.cursomc.resources.exceptions;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.ayamasita.cursomc.services.exceptions.ObjectNotFoundException;
+
+
+//classe auxiliar que intercepta as exceções
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class) //indica que é um tratador de exceção
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request)
+	{
+		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value() ,e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+		
+	}
+}
