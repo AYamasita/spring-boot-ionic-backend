cb8ab503ab3a0e2d966ef988c81562d823654382
ayamasita <alexandreyamasita@gmail.com>
2019-09-13 13:43:35 -0300

    inserindo uma nova Categoria com POST

--- src/main/java/com/ayamasita/cursomc/services/CategoriaService.java
+++ src/main/java/com/ayamasita/cursomc/services/CategoriaService.java
@@ -1,9 +1,12 @@
 package com.ayamasita.cursomc.services;
 
+import java.net.URI;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.ayamasita.cursomc.domain.Categoria;
 import com.ayamasita.cursomc.repositories.CategoriaRepository;
@@ -21,5 +24,11 @@ public class CategoriaService {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() 
 				-> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));	
-	}	
+	}		
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+		
+	}
 }
