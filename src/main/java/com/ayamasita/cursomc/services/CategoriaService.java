07da677e23bdaea9e59fca96c0708af665418776
ayamasita <alexandreyamasita@gmail.com>
2019-09-19 12:37:07 -0300

    Paginação com parâmetros opcionais na requisição

--- src/main/java/com/ayamasita/cursomc/services/CategoriaService.java
+++ src/main/java/com/ayamasita/cursomc/services/CategoriaService.java
@@ -6,6 +6,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.ayamasita.cursomc.domain.Categoria;
@@ -54,4 +57,11 @@ public class CategoriaService {
 	{
 		return repo.findAll();
 	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linerPerPage, String orderBy, String direction)
+	{
+		PageRequest pageRequest = PageRequest.of(page, linerPerPage,Direction.valueOf(direction),orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
 }
