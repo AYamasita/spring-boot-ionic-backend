ee41fecb855d43dadc1fe9f11202e5aac3c0275c
ayamasita <alexandreyamasita@gmail.com>
2019-09-24 09:34:12 -0300

    Inserindo um novo cliente com POST

--- src/main/java/com/ayamasita/cursomc/services/ClienteService.java
+++ src/main/java/com/ayamasita/cursomc/services/ClienteService.java
@@ -3,6 +3,8 @@ package com.ayamasita.cursomc.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
@@ -10,9 +12,15 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.ayamasita.cursomc.domain.Cidade;
 import com.ayamasita.cursomc.domain.Cliente;
+import com.ayamasita.cursomc.domain.Endereco;
+import com.ayamasita.cursomc.domain.enums.TipoCliente;
 import com.ayamasita.cursomc.dto.ClienteDTO;
+import com.ayamasita.cursomc.dto.ClienteNewDTO;
+
 import com.ayamasita.cursomc.repositories.ClienteRepository;
+import com.ayamasita.cursomc.repositories.EnderecoRepository;
 import com.ayamasita.cursomc.services.exceptions.DataIntegrityException;
 import com.ayamasita.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -21,6 +29,9 @@ public class ClienteService {
 	
 	@Autowired //automaticamente instanciada - injecao de dep. ou inversao controle
 	private ClienteRepository repo;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
 	
 	public Cliente find(Integer id)
 	{		
@@ -33,6 +44,37 @@ public class ClienteService {
 	public Cliente FromDTO( ClienteDTO objDTO) {
 		// TODO Auto-generated method stub
 		return new Cliente(objDTO.getId(),objDTO.getNome(),objDTO.getEmail(),null,null);
+		
+	}
+	
+	public Cliente FromDTO(ClienteNewDTO objDTO)
+	{
+		Cliente cli = new Cliente(null,objDTO.getNome(),objDTO.getEmail(),
+				objDTO.getCpfOuCnpj(),TipoCliente.ToEnum(objDTO.getTipo()));
+		
+		Cidade cid = new Cidade(objDTO.getCidadeId(),null,null);
+		
+		Endereco end = new Endereco(null,objDTO.getLogradouro(),objDTO.getNumero(),objDTO.getLogradouro(),
+				                    objDTO.getBairro(),objDTO.getCep(),
+				                    cli,cid);
+		cli.getEnderecos().add(end);
+		
+		cli.getTelefones().add(objDTO.getTelefone1());		
+		if (objDTO.getTelefone2() != null)
+			cli.getTelefones().add(objDTO.getTelefone2());
+		if (objDTO.getTelefone3() != null)
+			cli.getTelefones().add(objDTO.getTelefone3());
+		
+		return cli;
+	}
+	
+	@Transactional
+	public Cliente insert(Cliente obj)
+	{
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());		
+		return obj;
 	}
 
 	public Cliente update(Cliente obj) {
